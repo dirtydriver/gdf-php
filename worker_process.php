1cfb47b5fb7cc375181e81e88d631360ee3aa93b
petruskaakos <petruskaakos@outlook.com>
2018-05-06 15:10:57 +0200

    request

--- worker_process.php
+++ worker_process.php
@@ -1,15 +1,16 @@
 <?php
 include 'dbcon.php';
 
-$neve=$_GET['neve'];
-$email=$_GET['email'];
-$fizetes=$_GET['fizetes'];
-$agazat=$_GET['agazat'];
-$neme=$_GET['neme'];
+$neve=$_REQUEST['neve'];
+$email=$_REQUEST['email'];
+$fizetes=$_REQUEST['fizetes'];
+$agazat=$_REQUEST['agazat'];
+$neme=$_REQUEST['neme'];
 
 $insert_query="INSERT INTO `intalk2`.`dolgozo` (`neve`, `email`, `fizetes`, `agazat`, `neme`) VALUES ('$neve', '$email', '$fizetes', '$agazat', '$neme');";
 
 run_query($insert_query,$conn);
 
-header("index.php")
+header("index.php");
+exit();
 ?>
